d081ec50819ccfa7c87ff4f723da2c1ea767f9ba
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-09-28 23:20:38 -0500

    Agregar y Actualizar Habitaciones

--- src/main/java/pe/edu/cibertec/DAWII_CL1_GRUPO/controller/HabitacionController.java
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_GRUPO/controller/HabitacionController.java
@@ -3,17 +3,17 @@ package pe.edu.cibertec.DAWII_CL1_GRUPO.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.cibertec.DAWII_CL1_GRUPO.model.bd.Habitacion;
+import pe.edu.cibertec.DAWII_CL1_GRUPO.model.request.HabitacionRequest;
+import pe.edu.cibertec.DAWII_CL1_GRUPO.model.response.ResultadoResponse;
 import pe.edu.cibertec.DAWII_CL1_GRUPO.service.HabitacionService;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Controller
-@RequestMapping("habitacion")
+@RequestMapping("/habitacion")
 public class HabitacionController {
 
     private HabitacionService habitacionService;
@@ -28,6 +28,12 @@ public class HabitacionController {
     @ResponseBody
     public List<Habitacion> listarHabitaciones(){
         return habitacionService.listarHabitaciones();
+    }
+
+    @PostMapping("/guardar")
+    @ResponseBody
+    public ResultadoResponse guardarHabitacion(@RequestBody HabitacionRequest habitacionRequest){
+        return habitacionService.guardarHabitacion(habitacionRequest);
     }
 
 }
--- src/main/java/pe/edu/cibertec/DAWII_CL1_GRUPO/service/HabitacionService.java
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_GRUPO/service/HabitacionService.java
@@ -3,6 +3,9 @@ package pe.edu.cibertec.DAWII_CL1_GRUPO.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pe.edu.cibertec.DAWII_CL1_GRUPO.model.bd.Habitacion;
+import pe.edu.cibertec.DAWII_CL1_GRUPO.model.bd.Hotel;
+import pe.edu.cibertec.DAWII_CL1_GRUPO.model.request.HabitacionRequest;
+import pe.edu.cibertec.DAWII_CL1_GRUPO.model.response.ResultadoResponse;
 import pe.edu.cibertec.DAWII_CL1_GRUPO.repository.HabitacionRepository;
 
 import java.util.List;
@@ -15,6 +18,35 @@ public class HabitacionService {
 
     public List<Habitacion> listarHabitaciones(){
         return habitacionRepository.findAll();
+    }
+
+    public ResultadoResponse guardarHabitacion(HabitacionRequest habitacion){
+        String mensaje = "Habitacion registrado correctamente";
+        Boolean respuesta = true;
+        try{
+            Habitacion objHab = new Habitacion();
+            if(habitacion.getHabitacionId() > 0){
+                objHab.setHabitacionId(habitacion.getHabitacionId());
+            }
+            objHab.setNumeroHabitacion(habitacion.getNumeroHabitacion());
+            objHab.setTipo(habitacion.getTipo());
+            objHab.setCapacidad(habitacion.getCapacidad());
+            objHab.setPrecioNoche(habitacion.getPrecioNoche());
+            Boolean disponible = false;
+            if(habitacion.getDisponible() != null) {
+                disponible = habitacion.getDisponible();
+            }
+            objHab.setDisponible(disponible);
+            objHab.setFecha_ultima_mantenimiento(habitacion.getFecha_ultima_mantenimiento());
+            Hotel hotel = new Hotel();
+            hotel.setHotelId(habitacion.getHotelId());
+            objHab.setHotel(hotel);
+            habitacionRepository.save(objHab);
+        }catch (Exception ex){
+            mensaje = "Habitacion no registrada";
+            respuesta = false;
+        }
+        return ResultadoResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
     }
 
 }
