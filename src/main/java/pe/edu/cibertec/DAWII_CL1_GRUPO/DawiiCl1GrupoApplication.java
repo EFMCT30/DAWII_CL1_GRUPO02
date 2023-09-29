e4773247a62ff4cd864a1c2585c797f1cc5fc88c
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-09-28 19:50:51 -0500

    Primer Commit

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_GRUPO/DawiiCl1GrupoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_GRUPO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1GrupoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1GrupoApplication.class, args);
+	}
+
+}
